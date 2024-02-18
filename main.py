321242a8b2cc2fba94a5c633e8cc2716b053ca19
tanishkamagar <115335174+tanishka66@users.noreply.github.com>
2024-02-19 00:11:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import cv2 as cv
-
-sample = cv.imread('Sample3.jpg')
-cv.imshow("Sample", sample)
-
-gray = cv.cvtColor(sample, cv.COLOR_BGR2GRAY)
-
-threshold, thresh = cv.threshold(gray, 150, 255,cv.THRESH_BINARY)
-
-cv.imshow("Black And White", thresh)
-cv.waitKey(0)
+import cv2
+import numpy as np
+image1 = cv2.imread('sample.jpeg') 
+cv2.imshow('Otsu Threshold', image1) 
+img = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY) 
+ret, thresh1 = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)       
+cv2.imshow('Otsu Threshold', thresh1)
+if cv2.waitKey(0) & 0xff == 27: 
+    cv2.destroyAllWindows()
